2d0a9732e25737d5e3c2087f3ecea2aae2c126c6
ts-jichao.b.li <ts-jichao.b.li@rakuten.com>
2018-12-05 16:03:32 +0900

    TiddlyWiki

--- fgo/lib/plugins/authpdo/lang/el/lang.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-/**
- * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- *
- * @author Katerina Katapodi <extragold1234@hotmail.com>
- */
-$lang['connectfail']           = 'Δεν μπόρεσε να κάνει σύνδεση με την βάση δεδομένων';
