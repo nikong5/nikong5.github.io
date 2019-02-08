62336e9c938551c01f81aff0cae916bf37113a55
ts-jichao.b.li <ts-jichao.b.li@rakuten.com>
2019-02-08 12:47:57 +0900

    fgo item template done

--- js/nikong5.js
+++ js/nikong5.js
@@ -53,15 +53,15 @@ $(function(){
     $("#sub-header-include").load("../../sub-header.html #subPageheader");
     $("#sub-footer-include").load("../../footer.html #pagefooter");
 
-    // Search box
-    $("#searchBox").keyup(function () {
-        var value = $(this).val();
-        $("#dataSet li").each(function () {
-            $(this).hide();
-            if ($(this).text().indexOf($.trim(value)) >= 0) {
-                $(this).show();
-            }
-        });
-    });
+    // // Search box
+    // $("#searchBox").keyup(function () {
+    //     var value = $(this).val();
+    //     $("#dataSet li").each(function () {
+    //         $(this).hide();
+    //         if ($(this).text().indexOf($.trim(value)) >= 0) {
+    //             $(this).show();
+    //         }
+    //     });
+    // });
     
 });
