49358e312844c4eadf1702ea63928c90ef414833
ts-jichao.b.li <ts-jichao.b.li@rakuten.com>
2019-03-01 17:27:43 +0900

    test search box

--- js/nikong5.js
+++ js/nikong5.js
@@ -58,15 +58,45 @@ $(function(){
     $("#top-button-include").load("../../top-button.html #topButton");
 
     // Search box
-    $("#searchBox").keyup(function () {
-        var value = $(this).val();
-        $("#dataSet li").each(function () {
-            $(this).hide();
-            if ($(this).text().indexOf($.trim(value)) >= 0) {
-                $(this).show();
+    var current_key = undefined;
+    $("#searchBox").keydown(function(event){
+        $("#searchBox").focus();
+        if(event.key) {
+            if(event.key !== "Dead"){
+                current_key = event.key;
             }
-        });
+        }
+    }).keyup(function(event){
+        var value = $(this).val();
+        if(event.key === current_key) {
+            $("#dataSet li").each(function () {
+                $(this).hide();
+                if ($(this).text().indexOf($.trim(value)) >= 0) {
+                    $(this).show();
+                }
+            });
+        }
+        current_key = undefined;
     });
+    // $("#searchBox").blur(function(){
+    //     if(current_key !== undefined){
+    //         alert("222");
+    //     }
+    //     current_key = undefined;
+    // });
+
+
+
+    // $("#searchBox").keyup(function () {
+    //     var value = $(this).val();
+    //     $("#dataSet li").each(function () {
+    //         $(this).hide();
+    //         if ($(this).text().indexOf($.trim(value)) >= 0) {
+    //             $(this).show();
+    //         }
+    //     });
+    // });
+    
 
     // $("#searchBox").focus(function () {
     //     var value = $("#searchBox").val();
