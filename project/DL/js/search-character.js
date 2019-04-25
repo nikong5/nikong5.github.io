fc5be5bde9afd84e4b265a5e4627b43c7bdd4a29
ts-jichao.b.li <ts-jichao.b.li@rakuten.com>
2019-04-25 15:12:54 +0900

    dl add select bar

--- project/DL/js/search-character.js
+++ project/DL/js/search-character.js
@@ -1,3 +1,4 @@
+/*↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓ ALL ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓*/
 $(function() {
 	$("#table-character").bootstrapTable(characterFunc());
 
@@ -32,29 +33,28 @@ $(function() {
 					var strAtk = objArr[7];
 					var strType = objArr[2];
 
-					// if(strWeapon=="Icon_Weapon_Sword.png"){
-						obj["icon"] = "<img src='img/character/"+strIcon+"' width='23' height='23'>";
-						obj["name"] = strName;
-						obj["rare"] = "<img src='img/character/"+strRare+"' width='13' height='13'>";
-						obj["element"] = "<img src='img/character/"+strElement+"' width='13' height='13'>";
-						obj["weapon"] = "<img src='img/character/"+strWeapon+"' width='13' height='13'>";
-						obj["skill1"] = "<img src='img/character/"+strSkill1+"' width='23' height='23'>"+"<br>"+"<nobr style='font-size:7px;margin-left:-10px;'>"+strSkill1Name+"</nobr>";
-						obj["skill2"] = "<img src='img/character/"+strSkill2+"' width='23' height='23'>"+"<br>"+"<nobr style='font-size:7px;margin-left:-10px;'>"+strSkill2Name+"</nobr>";
-						obj["ex"] = "<img src='img/character/"+strEx+"' width='23' height='23'>"+"<br>"+"<nobr style='font-size:7px;margin-left:-10px;color:red;'>"+strExName+"</nobr>";
-						obj["ability1"] = "<img src='img/character/"+strAbility1+"' width='23' height='23'>"+"<br>"+"<nobr style='font-size:7px;margin-left:-10px;'>"+strAbility1Name+"</nobr>";
-						obj["ability2"] = "<img src='img/character/"+strAbility2+"' width='23' height='23'>"+"<br>"+"<nobr style='font-size:7px;margin-left:-10px;'>"+strAbility2Name+"</nobr>";
-						obj["ability3"] = "<img src='img/character/"+strAbility3+"' width='23' height='23'>"+"<br>"+"<nobr style='font-size:7px;margin-left:-10px;'>"+strAbility3Name+"</nobr>";
-						obj["hp"] = strHp;
-						obj["atk"] = strAtk;
-						obj["type"] = "<img src='img/character/"+strType+"' width='15' height='15'>";
-						dataList.push(obj);
-					// }
+					obj["icon"] = "<img src='img/character/"+strIcon+"' width='23' height='23'>";
+					obj["name"] = strName;
+					obj["rare"] = "<img src='img/character/"+strRare+"' width='13' height='13'>";
+					obj["element"] = "<img src='img/character/"+strElement+"' width='13' height='13'>";
+					obj["weapon"] = "<img src='img/character/"+strWeapon+"' width='13' height='13'>";
+					obj["skill1"] = "<img src='img/character/"+strSkill1+"' width='23' height='23'>"+"<br>"+"<nobr style='font-size:7px;margin-left:-10px;'>"+strSkill1Name+"</nobr>";
+					obj["skill2"] = "<img src='img/character/"+strSkill2+"' width='23' height='23'>"+"<br>"+"<nobr style='font-size:7px;margin-left:-10px;'>"+strSkill2Name+"</nobr>";
+					obj["ex"] = "<img src='img/character/"+strEx+"' width='23' height='23'>"+"<br>"+"<nobr style='font-size:7px;margin-left:-10px;color:red;'>"+strExName+"</nobr>";
+					obj["ability1"] = "<img src='img/character/"+strAbility1+"' width='23' height='23'>"+"<br>"+"<nobr style='font-size:7px;margin-left:-10px;'>"+strAbility1Name+"</nobr>";
+					obj["ability2"] = "<img src='img/character/"+strAbility2+"' width='23' height='23'>"+"<br>"+"<nobr style='font-size:7px;margin-left:-10px;'>"+strAbility2Name+"</nobr>";
+					obj["ability3"] = "<img src='img/character/"+strAbility3+"' width='23' height='23'>"+"<br>"+"<nobr style='font-size:7px;margin-left:-10px;'>"+strAbility3Name+"</nobr>";
+					obj["hp"] = strHp;
+					obj["atk"] = strAtk;
+					obj["type"] = "<img src='img/character/"+strType+"' width='15' height='15'>";
+					dataList.push(obj);
 				}
 			}
 		}
 		console.log(dataList);
+
 		$("#table-character").bootstrapTable("load", dataList);
-    });
+  });
 });
 
 function characterFunc() {
@@ -65,7 +65,7 @@ function characterFunc() {
 		search: true, //是否启用搜索框
 		uniqueId: "icon",
 		sortable: true, //是否启用排序
-		sortOrder: "asc", //排序方式
+		sortOrder: "desc", //排序方式
 		sortName: "icon", //需要排序的字段
 		columns: [
 			{
@@ -141,3 +141,84 @@ function characterFunc() {
 	}
 	return table;
 };
+/*↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑ ALL ↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑*/
+
+/*↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓ BRANCH SELECT ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓*/
+function characterFuncBranchSelect(selElement,selWeapon) {
+	$("#table-character").bootstrapTable(characterFunc());
+
+	$.get('input/character.csv',function(data){
+		var dataList = [];
+		var csv = $.csv.toArrays(data);
+		if(!$.isEmptyObject(csv) && csv.length > 1) {
+			for(var key = 1, len = csv.length; key < len; key++) {
+				var values = csv[key];
+				if(!$.isEmptyObject(values)) {
+					var obj = {};
+					var objArr = (String(values)).split(",");
+					
+					var strIcon = objArr[1];
+					var strName = objArr[0];
+					var strRare = objArr[3];
+					var strElement = objArr[4];
+					var strWeapon = objArr[5];
+					var strSkill1 = objArr[8];
+					var strSkill1Name = objArr[9];
+					var strSkill2 = objArr[10];
+					var strSkill2Name = objArr[11];
+					var strEx = objArr[12];
+					var strExName = objArr[13];
+					var strAbility1 = objArr[14];
+					var strAbility1Name = objArr[15];
+					var strAbility2 = objArr[16];
+					var strAbility2Name = objArr[17];
+					var strAbility3 = objArr[18];
+					var strAbility3Name = objArr[19];
+					var strHp = objArr[6];
+					var strAtk = objArr[7];
+					var strType = objArr[2];
+
+					if(strElement==selElement){
+						obj["icon"] = "<img src='img/character/"+strIcon+"' width='23' height='23'>";
+						obj["name"] = strName;
+						obj["rare"] = "<img src='img/character/"+strRare+"' width='13' height='13'>";
+						obj["element"] = "<img src='img/character/"+strElement+"' width='13' height='13'>";
+						obj["weapon"] = "<img src='img/character/"+strWeapon+"' width='13' height='13'>";
+						obj["skill1"] = "<img src='img/character/"+strSkill1+"' width='23' height='23'>"+"<br>"+"<nobr style='font-size:7px;margin-left:-10px;'>"+strSkill1Name+"</nobr>";
+						obj["skill2"] = "<img src='img/character/"+strSkill2+"' width='23' height='23'>"+"<br>"+"<nobr style='font-size:7px;margin-left:-10px;'>"+strSkill2Name+"</nobr>";
+						obj["ex"] = "<img src='img/character/"+strEx+"' width='23' height='23'>"+"<br>"+"<nobr style='font-size:7px;margin-left:-10px;color:red;'>"+strExName+"</nobr>";
+						obj["ability1"] = "<img src='img/character/"+strAbility1+"' width='23' height='23'>"+"<br>"+"<nobr style='font-size:7px;margin-left:-10px;'>"+strAbility1Name+"</nobr>";
+						obj["ability2"] = "<img src='img/character/"+strAbility2+"' width='23' height='23'>"+"<br>"+"<nobr style='font-size:7px;margin-left:-10px;'>"+strAbility2Name+"</nobr>";
+						obj["ability3"] = "<img src='img/character/"+strAbility3+"' width='23' height='23'>"+"<br>"+"<nobr style='font-size:7px;margin-left:-10px;'>"+strAbility3Name+"</nobr>";
+						obj["hp"] = strHp;
+						obj["atk"] = strAtk;
+						obj["type"] = "<img src='img/character/"+strType+"' width='15' height='15'>";
+						dataList.push(obj);
+					}
+
+					if(strWeapon==selWeapon){
+						obj["icon"] = "<img src='img/character/"+strIcon+"' width='23' height='23'>";
+						obj["name"] = strName;
+						obj["rare"] = "<img src='img/character/"+strRare+"' width='13' height='13'>";
+						obj["element"] = "<img src='img/character/"+strElement+"' width='13' height='13'>";
+						obj["weapon"] = "<img src='img/character/"+strWeapon+"' width='13' height='13'>";
+						obj["skill1"] = "<img src='img/character/"+strSkill1+"' width='23' height='23'>"+"<br>"+"<nobr style='font-size:7px;margin-left:-10px;'>"+strSkill1Name+"</nobr>";
+						obj["skill2"] = "<img src='img/character/"+strSkill2+"' width='23' height='23'>"+"<br>"+"<nobr style='font-size:7px;margin-left:-10px;'>"+strSkill2Name+"</nobr>";
+						obj["ex"] = "<img src='img/character/"+strEx+"' width='23' height='23'>"+"<br>"+"<nobr style='font-size:7px;margin-left:-10px;color:red;'>"+strExName+"</nobr>";
+						obj["ability1"] = "<img src='img/character/"+strAbility1+"' width='23' height='23'>"+"<br>"+"<nobr style='font-size:7px;margin-left:-10px;'>"+strAbility1Name+"</nobr>";
+						obj["ability2"] = "<img src='img/character/"+strAbility2+"' width='23' height='23'>"+"<br>"+"<nobr style='font-size:7px;margin-left:-10px;'>"+strAbility2Name+"</nobr>";
+						obj["ability3"] = "<img src='img/character/"+strAbility3+"' width='23' height='23'>"+"<br>"+"<nobr style='font-size:7px;margin-left:-10px;'>"+strAbility3Name+"</nobr>";
+						obj["hp"] = strHp;
+						obj["atk"] = strAtk;
+						obj["type"] = "<img src='img/character/"+strType+"' width='15' height='15'>";
+						dataList.push(obj);
+					}
+				}
+			}
+		}
+		console.log(dataList);
+
+		$("#table-character").bootstrapTable("load", dataList);
+  });
+}
+/*↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑ BRANCH SELECT ↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑*/
